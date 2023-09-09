4de2fbc0422a8b944b18eadddd5b3b4fcab6a8ef
Drodriguez87 <drodriguezal@soy.sena.edu.co>
2023-09-08 22:50:07 -0500

    Cambios en produccion

--- controllers/Productions.php
+++ controllers/Productions.php
@@ -1,25 +1,56 @@
 <?php
-require_once "models/Production.php";  
+    require_once "models/Production.php";  
 
-class Productions {
-    public function main() {}
+    class Productions {
+        public function main() {}
     
-    // Crear Produccion
-    public function productionCreate(){
-        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            echo "Formulario de crear produccion";
+        // Crear Produccion
+        public function productionCreate(){
+            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                echo "Formulario de crear produccion";
+            }
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $production = new Production(
+                1,
+                "Chaqueta de cuero",
+                10,
+                12,
+                1,
+                "Chaqueta"
+            );
+            $production->createProduction();
+            header("Location:?c=Production&a=rolProduction");
+            }
         }
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $production = new Production(
-            1,
-            "Chaqueta de cuero",
-            10,
-            12,
-            1,
-            "Chaqueta"
-        );
-        $production->createProduction();
-        header("Location:?c=Production&a=rolProduction")
+        // Consultar Produccion
+        public function productionRead(){            
+            $productions = new Production();
+            $productions = $productions->readProduction();
+            echo "Formulario de Consultar produccion";
+        }
+        // Actualizar Produccion
+        public function productionUpdate(){
+            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                $production = new Production();
+                $production = $production->getProdById($_GET['prodCode']);
+                echo "Formulario de Actualización de produccion";                
+            }
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $production = new Production(
+                    $_POST['prodCode'],
+                    $_POST['prodDescription'],
+                    $_POST['prodDate'],
+                    $_POST['prodAmount'],
+                    $_POST['prodId'],
+                    $_POST['prodName']                    
+                );            
+                $production->updateProduction();
+            }
+        }
+        // Eliminar Roles
+        public function productionDelete() {
+            $prod = new Production;
+            $prod->deleteprod($_GET['prodCode']);
+        }
     }
-}
 ?>
